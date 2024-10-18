027e849edbb2e9f9e74640b38a70f66ff2f3ed6f
Jared Busch <sorvani@gmail.com>
2024-10-17 19:09:10 -0500

    Update root_setup.sh

--- root_setup.sh
+++ root_setup.sh
@@ -4,7 +4,7 @@
 
 # collect the linux username and github username of the person runnning the script
 read -p "Enter a new username to use for SSH access: " myUserName
-read -p "Enter the github usernamer of $myUserName: " myGitName
+read -p "Enter the github username for $myUserName: " myGitName
 
 # Create user account with default password of ChangeMe
 echo "Creating the user $myUserName and assigning permissions"
@@ -12,14 +12,8 @@ echo ""
 useradd --create-home $myUserName --password $(openssl passwd -1 ChangeMe) --shell /bin/bash>> setup.log
 # expire the password to force reset on first login
 chage -d 0 $myUserName >> setup.log
-# Add user to wheel and asterisk groups
+# Add user to sudo
 gpasswd -a $myUserName sudo >> setup.log
-#asterisk does not yet exist. move to later
-#gpasswd -a $myUserName asterisk >> setup.log
-# get the ssh-key-sync application form github
-wget https://github.com/shoenig/ssh-key-sync/releases/download/v1.7.2/ssh-key-sync_1.7.2_linux_amd64.tar.gz
-# install  ssh-key-syn
-tar -C /usr/local/bin -xf ssh-key-sync_1.7.2_linux_amd64.tar.gz
 # Create the user's .ssh folder 
 mkdir /home/$myUserName/.ssh
 # Set permissions
@@ -28,6 +22,10 @@ chmod 700 /home/$myUserName/.ssh
 touch /home/$myUserName/.ssh/authorized_keys
 # set permissions
 chmod 600 /home/$myUserName/.ssh/authorized_keys
+# get the ssh-key-sync application form github
+wget https://github.com/shoenig/ssh-key-sync/releases/download/v1.7.2/ssh-key-sync_1.7.2_linux_amd64.tar.gz
+# install  ssh-key-syn
+tar -C /usr/local/bin -xf ssh-key-sync_1.7.2_linux_amd64.tar.gz
 # execute ssh-key-sync to get the pub keys for this user
 ssh-key-sync --github-user $myGitName --system-user $myUserName
 chown -R $myUserName:$myUserName /home/$myUserName
@@ -37,5 +35,5 @@ ipaddress=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([
 echo "Root setup complete."
 echo "Please log out from the root user and login, via SSH to $ipaddress or the FQDN you have setup, with username: $myUserName."
 echo "You will be required to change your password. It is currently set to: ChangeMe"
-echo ""
-# echo "Then execute 'sudo ./setup.sh'"
+echo "You will be disconnected upon successfully setting your password."
+echo "Log in again with SSH, switch to the root user, and run the FreePBX 17 installation script."
